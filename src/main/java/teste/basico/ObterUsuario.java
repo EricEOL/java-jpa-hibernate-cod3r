217fb8a67c97a0d10cffdc8c2f532d6581c7732f
EricEOL <lopes.eric051@gmail.com>
2021-12-28 14:01:48 -0300

    Obtendo todos os registros do banco de dados com jpa

--- src/main/java/teste/basico/ObterUsuario.java
+++ src/main/java/teste/basico/ObterUsuario.java
@@ -6,23 +6,20 @@ import javax.persistence.Persistence;
 
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
-	
+public class ObterUsuario {
+
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.
 				createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario novoUsuario = new Usuario("Thamiris", "thamiris@gmail.com");
-		
-		em.getTransaction().begin();
-		em.persist(novoUsuario);
-		em.getTransaction().commit();
-		
-		System.out.println("O id gerado foi: " + novoUsuario.getId());
+		Usuario usuario = em.find(Usuario.class, 3L);
+		System.out.println(usuario.getNome());
 		
 		em.close();
 		emf.close();
+		
 	}
+	
 }
