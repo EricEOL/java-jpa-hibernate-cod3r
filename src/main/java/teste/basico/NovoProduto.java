f0b4910540c46e31bc677a1decf90483dd521144
EricEOL <lopes.eric051@gmail.com>
2021-12-29 21:49:36 -0300

    Criando um DAO para abstração da inserção de dados no db

---
+++ src/main/java/teste/basico/NovoProduto.java
@@ -0,0 +1,20 @@
+package teste.basico;
+
+import infra.DAO;
+import modelo.basico.Produto;
+
+public class NovoProduto {
+
+	public static void main(String[] args) {
+		
+		DAO<Produto> dao = new DAO(Produto.class);
+		
+		Produto produto = new Produto("Curso Analista de Gráfico", 2000.00);
+		
+		dao.incluirAtomico(produto);
+		
+		dao.fechar();
+
+	}
+
+}
--- src/main/java/teste/basico/NovoUsuario.java
+++ src/main/java/teste/basico/NovoUsuario.java
@@ -1,4 +1,4 @@
-package teste.basico;
+  package teste.basico;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
